5983792f6bcd725dc4aa976b423a40bcc151751a
Ruslan Ivanov <ruslan4432013@gmail.com>
2022-06-09 20:36:09 +0300

    second task is done

--- practical_1/script.js
+++ practical_1/script.js
@@ -24,3 +24,11 @@
 'use strict';
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '')
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
