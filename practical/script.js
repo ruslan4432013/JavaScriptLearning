9548c8ac338963c0ce9c5732e7659ca0c826b0fa
Ruslan Ivanov <ruslan4432013@gmail.com>
2022-06-10 17:17:27 +0300

    step 3

--- practical/script.js
+++ practical/script.js
@@ -14,8 +14,16 @@
 
 
 'use strict';
+let numberOfFilms;
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+
+    while (!numberOfFilms || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+    }
+}
+
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -26,39 +34,59 @@ let personalMovieDB = {
 }
 
 
+function rememberMyFilm() {
+    for (let i = 0; i < 2; i++) {
+        let lastFilmWatched = prompt('Один из последних просмотренных фильмов? Введите (exit) для выхода ', '')
 
-for(let i = 0; i < 2; i++) {
-    let lastFilmWatched = prompt('Один из последних просмотренных фильмов? Введите (exit) для выхода ', '')
 
+        if (!lastFilmWatched || lastFilmWatched.length > 50) {
+            alert('Последний просмотренный фильм не должен быть пустым или содержать больше 50 символов')
+            i--
+            continue
+        }
 
-    if(!lastFilmWatched || lastFilmWatched.length > 50) {
-        alert('Последний просмотренный фильм не должен быть пустым или содержать больше 50 символов')
-        i--
-        continue
+        let ratingLastFilmWatched = prompt('На сколько оцените его? Введите (exit) для выхода ', '')
+
+        if (!ratingLastFilmWatched) {
+            alert('Рейтинг не может быть пустым')
+            i--
+            continue
+        }
+
+        personalMovieDB.movies[lastFilmWatched] = ratingLastFilmWatched
+
     }
-
-    let ratingLastFilmWatched = prompt('На сколько оцените его? Введите (exit) для выхода ', '')
-
-    if (!ratingLastFilmWatched) {
-        alert('Рейтинг не может быть пустым')
-        i--
-        continue
-    }
-
-    personalMovieDB.movies[lastFilmWatched] = ratingLastFilmWatched
-
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов')
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель')
-} else if (personalMovieDB.count >= 30) {
-    alert('Вы киноман')
-} else {
-    alert('Ошибка')
+rememberMyFilm()
+
+
+function detectPersonalLevel() {
+
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов')
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель')
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман')
+    } else {
+        alert('Ошибка')
+    }
 }
 
+detectPersonalLevel()
 
+function showMyDb(hide) {
+    if (!hide) {
+        console.log(personalMovieDB)
+    }
+}
+
+function writeYourGenres() {
+    for(let i=1; i <= 3; i++) {
+        let loveGenres = prompt(`Ваш любимый жанр под номером ${i}`)
+        personalMovieDB.genres.push(loveGenres)
+    }
+}
 
 console.log(personalMovieDB)
